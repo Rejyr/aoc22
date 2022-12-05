8b2d914ff03417c386d6b51c292b27ee58cb570f
Rejyr <jerrylwang123@gmail.com>
2022-12-05 13:20:32 -0500

    refactor: day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,42 +1,19 @@
-use std::vec;
-
 use crate::util::input;
 
+fn calories() -> Vec<i32> {
+    let mut calories: Vec<i32> = input()
+        .split("\n\n")
+        .map(|s| s.lines().map(|s| s.parse::<i32>().unwrap()).sum())
+        .collect();
+    calories.sort_unstable();
+    calories.reverse();
+    calories
+}
+
 pub fn solution1() {
-    let input = input();
-
-    let most_calories = input
-        .lines()
-        .map(|s| s.parse().unwrap_or(0))
-        // (max, sum)
-        .fold((0, 0), |acc, x| {
-            if x == 0 {
-                return (std::cmp::max(acc.1, acc.0), 0);
-            }
-
-            (acc.0, acc.1 + x)
-        })
-        .0;
-    println!("{most_calories}")
+    println!("{}", calories()[0]);
 }
 
 pub fn solution2() {
-    let input = input();
-    let mut calories = vec![];
-
-    let last = input
-        .lines()
-        .map(|s| s.parse().unwrap_or(0))
-        .fold(0, |acc, x| {
-            if x == 0 {
-                calories.push(acc);
-                0
-            } else {
-                acc + x
-            }
-        });
-    calories.push(last);
-    calories.sort_unstable();
-    let calories: i32 = calories.iter().rev().take(3).sum();
-    println!("{calories}");
+    println!("{}", calories().iter().take(3).sum::<i32>());
 }
